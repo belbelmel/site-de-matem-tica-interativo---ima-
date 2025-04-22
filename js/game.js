f878f1b98197bbc5da2017fd37332e1dfe4e2057
belbelmel <163556027+belbelmel@users.noreply.github.com>
2025-04-22 15:01:09 +0000

    alterações no projeto

--- js/game.js
+++ js/game.js
@@ -1,18 +1,26 @@
 // Variáveis principais do jogo
 let respostaCorreta;
 let pontuacao = 0;
-let modoAtual = "basico";
+let modoAtual = 'basico';
+
+// Função que inicia jogo com o modo escolhido
+function iniciarJogo(modo) {
+modoAtual = modo;
+document.getElementById("area-jogo").style.display = "block";
+novaPergunta();
+}
 
 // Função que gera nova pergunta e mostra na tela
 function novaPergunta() {
     const numeros = gerarNumeros(modoAtual);
     const operacao = escolherOperacao();
-    const perguntaTexto = '${numeros.num1} ${operacao} ${numeros.num2}';
+    const perguntaTexto =`${numeros.num1} ${operacao} ${numeros.num2}`;
 
     respostaCorreta = calcularResposta(numeros.num1, numeros.num2, operacao);
 
     document.getElementById("pergunta").innerText = perguntaTexto;
     document.getElementById("resposta").value = "";
+    documente.getElementById("resultado").innerText = "";
 }
 
 // Gera números aleatórios, maiores no modo desafio
